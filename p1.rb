92fb552a227786c5e9d02b1c96ae655dae5e48ef
Ryan Miller <miller2300@comcast.net>
2012-04-30 15:05:13 -0400

    testing P1 class

--- p1.rb
+++ p1.rb
@@ -1,12 +1,23 @@
 # If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
 # The sum of these multiples is 23.
 # Find the sum of all the multiples of 3 or 5 below 1000.
-naturals = []
 
-(1..100).each {|i| 
-  if i % 3 == 0 || i % 5 == 0 then 
-    naturals.push(i)
+class Problem1
+
+  def initialize
+    @naturals = []
   end
-}
 
-puts naturals.inject(0){|sum,item| sum + item}
+  def sum_multiples_of_3_or_5_below n
+
+    (1...n).each { |i|
+      if i % 3 == 0 || i % 5 == 0
+        @naturals.push(i)
+      end
+    }
+
+    @naturals.inject(0) { |sum, item | sum + item }
+
+  end
+
+end
---
+++ spec/p1_spec.rb
@@ -0,0 +1,23 @@
+require_relative '../p1'
+
+describe Problem1 do
+
+  before :each do
+    @p1 = Problem1.new
+  end
+
+  describe "#new" do
+    it "takes no parameters and returns a P1 object" do
+      @p1.should be_an_instance_of Problem1
+    end
+  end
+
+  it "it returns 23 when looking at first 10 naturals" do
+    @p1.sum_multiples_of_3_or_5_below(10).should eql 23
+  end
+
+  it "it returns 233168 when looking at first 1000 naturals" do
+    @p1.sum_multiples_of_3_or_5_below(1000).should eql 233168
+  end
+
+end
